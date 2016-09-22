7ee2edfccf629cf4638bfe43fb120ac85fd5f093
Fan <fancheung@outlook.com>
2016-09-22 15:35:32 +0800

    async and await refactoring

--- api.test.ts
+++ api.test.ts
@@ -48,39 +48,80 @@ class TestRunner {
         });
     }
 
-    login(done) {
+    async login(done) {
+
         MongoClient.connect(DB_URL, (err, result) => {
             db = result;
             this.users = db.collection('users')
             this.users.deleteMany({})
         });
-        api.post('/api/register').send({ user_email: TestRunner.USER_DATA.email, user_password: TestRunner.USER_DATA.password }).expect(200, (error, result) => {
 
-            if (error)
-                throw error
+        try {
+            await new Promise(resolve => {
+                api.post('/api/register').send({ user_email: TestRunner.USER_DATA.email, user_password: TestRunner.USER_DATA.password }).expect(200, (error, result) => {
+                    if (error)
+                        throw error
+                    if (!result)
+                        throw 'Register failed'
+                    return resolve(result)
+                })
+            })
 
-            if (!result)
-                throw 'Cannot find user'
+            this.authorizedUser = await new Promise(resolve => {
+                this.users.findOne({ email: TestRunner.USER_DATA.email }, (err, result) => {
+                    this.authorizedUser = result;
+                    // activate the account
+                    return resolve(result)
+                })
+            })
 
-            assert.equal(result.body.data.userEmail, TestRunner.USER_DATA.email)
-            this.users.findOne({ email: TestRunner.USER_DATA.email }, (err, result) => {
-                this.authorizedUser = result;
-                // activate the account
-                let activationLink = '/api/activate/?email=' + this.authorizedUser.email + '&token=' + this.authorizedUser.email_token;
-                api.get(activationLink).expect(200, (error, res)=>{
+            let activationLink = '/api/activate/?email=' + this.authorizedUser.email + '&token=' + this.authorizedUser.email_token;
+
+            await new Promise(resolve => {
+                api.get(activationLink).expect(200, (error, res) => {
                     assert.equal(res.body.data.email_token, null, 'why the toekn still in db!!');
                     assert.equal(res.body.data.email_expires, 0, 'Should expire expire');
-
-                    api.post('/api/login').send({ username: TestRunner.USER_DATA.email, password: TestRunner.USER_DATA.password }).expect(200, (err, result) => {
-                        this.token = result.body.data.token
-                        assert(result.body.data.token, 'jwt token not available');
-                        assert(result.body.data.uid, 'uid not available');
-                        done();
-
-                    });
+                    resolve(true)
                 })
-            });
-        })
+            })
+
+            this.token = await new Promise(resolve => {
+                api.post('/api/login').send({ username: TestRunner.USER_DATA.email, password: TestRunner.USER_DATA.password }).expect(200, (err, result) => {
+                    assert(result.body.data.token, 'jwt token not available');
+                    assert(result.body.data.uid, 'uid not available');
+                    return resolve(result.body.data.token)
+                })
+            })
+            done()
+        } catch (e) {
+            throw e
+        }
+        //     api.post('/api/register').send({ user_email: TestRunner.USER_DATA.email, user_password: TestRunner.USER_DATA.password }).expect(200, (error, result) => {
+        //
+        //         if (error)
+        //             throw error
+        //         if (!result)
+        //             throw 'Register failed'
+        //
+        //
+        //         assert.equal(result.body.data.userEmail, TestRunner.USER_DATA.email)
+        //
+        //         this.users.findOne({ email: TestRunner.USER_DATA.email }, (err, result) => {
+        //             this.authorizedUser = result;
+        //             // activate the account
+        //             let activationLink = '/api/activate/?email=' + this.authorizedUser.email + '&token=' + this.authorizedUser.email_token;
+        //             api.get(activationLink).expect(200, (error, res) => {
+        //                 assert.equal(res.body.data.email_token, null, 'why the toekn still in db!!');
+        //                 assert.equal(res.body.data.email_expires, 0, 'Should expire expire');
+        //                 api.post('/api/login').send({ username: TestRunner.USER_DATA.email, password: TestRunner.USER_DATA.password }).expect(200, (err, result) => {
+        //                     this.token = result.body.data.token
+        //                     assert(result.body.data.token, 'jwt token not available');
+        //                     assert(result.body.data.uid, 'uid not available');
+        //                     done();
+        //                 });
+        //             })
+        //         });
+        //     })
     }
 
 
